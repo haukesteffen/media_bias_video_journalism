1bdd755dcb531b4dfff8d74945078d8b5b667d98
haukesteffen <57219809+haukesteffen@users.noreply.github.com>
2022-05-17 19:17:56 +0200

    Add parallel scraping
    OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES

--- src/main.py
+++ src/main.py
@@ -1,22 +1,42 @@
-import warnings
-warnings.filterwarnings('ignore')
-
-from utils import get_N_matrix, filter_N_by_information_score, sort_topics, extract_topics
 import pandas as pd
-from sklearn.preprocessing import StandardScaler
-from sklearn.decomposition import TruncatedSVD
-import seaborn as sns
-import matplotlib.pyplot as plt
-import joblib
+import numpy as np
+from pandarallel import pandarallel
+from youtube_transcript_api import YouTubeTranscriptApi
+from youtubesearchpython import Playlist, Channel, Video, Transcript, playlist_from_channel_id
+from tqdm import tqdm
+tqdm.pandas()
+pandarallel.initialize(progress_bar=True)
 
+def fetch_video_transcript(video_id):
+    try:
+        return YouTubeTranscriptApi.get_transcript(video_id, languages=["de", 'en'])
+    except:
+        return np.nan
+
+def fetch_video_info(video_id):
+    info = Video.getInfo(video_id)
+    return [info["title"], info["duration"]["secondsText"], info["publishDate"], info["description"], info["category"]]
+
+def scrape_videos(channel_id):
+    df = pd.DataFrame(columns=["medium","title","id","duration","transcript","date","description","category"])
+    channel = Channel.get(channel_id)
+    
+    playlist = Playlist(playlist_from_channel_id(channel["id"]))
+    while playlist.hasMoreVideos:
+        playlist.getNextVideos()
+    df['id'] = [video.get("id") for video in tqdm(playlist.videos)]
+    df['transcript'] = df['id'].parallel_apply(fetch_video_transcript)
+    df['medium'] = channel.get("title")
+    df.loc[:, ["title","duration","date","description","category"]] = df['id'].parallel_apply(fetch_video_info).to_list()
+    return df
 
 channels = {
-    'junge Welt': 'UC_wVoja0mx0IFOP8s1nfRSg',
-    'ZEIT ONLINE': 'UC_YnP7DDnKzkkVl3BaHiZoQ',
-    'faz': 'UCcPcua2PF7hzik2TeOBx3uw',
-    'Süddeutsche Zeitung': 'UC6bx8B0W0x_5NQFAF3Nbd-A',
-    'NZZ Neue Zürcher Zeitung': 'UCK1aTcR0AckQRLTlK0c4fuQ',
-    'WELT': 'UCZMsvbAhhRblVGXmEXW8TSA',
+    #'junge Welt': 'UC_wVoja0mx0IFOP8s1nfRSg',
+    #'ZEIT ONLINE': 'UC_YnP7DDnKzkkVl3BaHiZoQ',
+    #'faz': 'UCcPcua2PF7hzik2TeOBx3uw',
+    #'Süddeutsche Zeitung': 'UC6bx8B0W0x_5NQFAF3Nbd-A',
+    #'NZZ Neue Zürcher Zeitung': 'UCK1aTcR0AckQRLTlK0c4fuQ',
+    #'WELT': 'UCZMsvbAhhRblVGXmEXW8TSA',
     'Bayerischer Rundfunk': 'UCZuFrqyZWfw_Zf0OnXWUXyQ',
     'Der Tagesspiegel': 'UCFemltyr6criZZsWFHUSHPQ',
     'Tagesschau': 'UC5NOEUbkLheQcaaRldYW5GA',
@@ -37,30 +57,10 @@ channels = {
 }
 
 def main():
-    topic = 'Wirtschaft'
-    N_df = get_N_matrix(topic=topic)
-    N_df = filter_N_by_information_score(N_df)
-    scaler = StandardScaler()
-    model = TruncatedSVD(n_components=3)
-
-    N_scaled = scaler.fit_transform(N_df.values)
-    N_df_trunc = model.fit_transform(N_scaled)
-
-    sns.set(palette="coolwarm", style='whitegrid')
-    sns.scatterplot(
-        model.components_[0], 
-        model.components_[1], 
-        hue=N_df.columns,
-        palette='coolwarm',
-        ).set(title=f'Thema "{topic}" - Hauptachsen 0 und 1')
-    plt.figure()
-    sns.scatterplot(
-        model.components_[1], 
-        model.components_[2], 
-        hue=N_df.columns,
-        palette='coolwarm',
-        ).set(title=f'Thema "{topic}" - Hauptachsen 1 und 2')
-    plt.show()
+    for name, id in channels.items():
+        df = scrape_videos(id)
+        medium = df.iloc[0]['medium']
+        df.to_pickle(f'data/raw/{medium}.pkl')
 
 if __name__ == "__main__":
     main()
